bd12a769ac4a1c00f0c8870498e2d2534f728b2f
William Liu <william.liu4518@gmail.com>
2022-07-30 15:34:37 -0600

    fix: smooth scrolling to anchor tags

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const mobileMenuBtn = document.getElementById("mobile-menu-cta")
 const mobileExitBtn = document.getElementById("mobile-exit-cta")
 const nav = document.querySelector("nav")
-const navLinks = document.querySelectorAll(".nav-item") 
+const navLinks = document.querySelectorAll('a[href^="#"]')
 
 mobileMenuBtn.addEventListener("click", () => {
     nav.classList.add("show")
@@ -11,12 +11,19 @@ mobileExitBtn.addEventListener("click", () => {
     nav.classList.remove("show")
 })
 
-navLinks.forEach(el => 
-    el.addEventListener("click", () => {
-        nav.classList.remove("show")
-    })
-)
+navLinks.forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        nav.classList.remove("show")    // Remove Hamburger Menu
 
-window.onbeforeunload = function () {
-    window.scrollTo(0,0);
-};
+        e.preventDefault();    // Resets def (anchor tags)
+
+        // Go to section smoothly by getting href
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// window.onbeforeunload = function () {
+//     window.scrollTo(0,0);
+// };
